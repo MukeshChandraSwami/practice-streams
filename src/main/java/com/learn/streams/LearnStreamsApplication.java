d7b06ec15952228cd42d167da0d3904e49733cce
mukesh swami <mukesh.swami.501598@gmail.com>
2022-03-13 17:01:53 +0530

    Added initial project structure

---
+++ src/main/java/com/learn/streams/LearnStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnStreamsApplication.class, args);
+	}
+
+}
